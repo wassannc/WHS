9df69ab319ca43eeacf544ac2f4beb8c0ca4cef8
wassannc <balaram@wassan.org>
2026-05-29 10:40:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if page == "Submission Matrix":
 
         df = load_odk_data(config["form_id"])
         st.write(form_name)
-        st.write(df.head())
+        st.write(df["enumerator-Enumerator_name"].head(20))
         
         if df.empty:
             continue
