753a40adb3fc624e31c0a77310d7d150231d0192
wassannc <balaram@wassan.org>
2026-06-03 13:14:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-import io
 
 ODK_URL = st.secrets["ODK_URL"]
 USERNAME = st.secrets["USERNAME"]
@@ -9,17 +8,19 @@ PASSWORD = st.secrets["PASSWORD"]
 PROJECT_ID = st.secrets["PROJECT_ID"]
 
 @st.cache_data(ttl=300)
-def load_odk_data(form_id):
-
+def load_data(form_id):
     url = f"{ODK_URL}/v1/projects/{PROJECT_ID}/forms/{form_id}/submissions.csv"
-
-    response = requests.get(
-        url,
-        auth=(USERNAME, PASSWORD)
-    )
+    
+    response = requests.get(url, auth=(USERNAME, PASSWORD))
 
     if response.status_code != 200:
-        st.error(f"{url} → {response.status_code}")
+        st.error(f"Error: {response.status_code}")
+        return pd.DataFrame()
+
+    import io
+
+    if response.status_code != 200:
+        st.error(f"Error: {response.status_code}")
         return pd.DataFrame()
 
     df = pd.read_csv(io.StringIO(response.text))
