3e5dbbd27047fc2e4a3a259e42e5ed7226bcd09f
wassannc <balaram@wassan.org>
2026-06-11 17:03:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,8 @@ elif page in FORMS:
                 "WSC Works",
                 "WC Works",
                 "LTCB Works",
-                "Guidewall repair"
+                "Guidewall repair",
+                "Guidewall Bedjoint leakage"
             ]
         )
         st.write("Selected:", report_type)
@@ -152,6 +153,41 @@ elif page in FORMS:
                     "avg_height_gwr",
                     "volume_guidewall_tobe_break",
                     "volume_guidewall_tobe_constrn"
+                ]
+            ]
+            st.dataframe(df)
+            st.stop()
+
+        elif report_type == "Guidewall Bedjoint Leakage":
+            df = load_repeat_data(
+                "2.Rejuvenation_works",
+                "Submissions.gwbjl.gwbjl_"
+            )  
+            main_df = load_data("2.Rejuvenation_works")
+            df = df.merge(
+                main_df[
+                    [
+                        "KEY",
+                        "basic_details_repairs-village",
+                        "basic_details_repairs-gp",
+                        "basic_details_repairs-block"
+                    ]
+                ],
+                left_on="__Submissions-id",
+                right_on="KEY",
+                how="left"
+            )
+            # SHOW ONLY REQUIRED COLUMNS
+            df = df[
+                [
+                    "basic_details_repairs-block",
+                    "basic_details_repairs-gp",
+                    "basic_details_repairs-village",
+                    "leakage_canal_length_gwbjl_leak1",
+                    "leakage_canal_breadth_gwbjl_leak1",
+                    "leakage_canal_height_gwbjl_leak1",
+                    "volume_cc_gwbjl_leak1",
+                    "total_volume_gwbjl"
                 ]
             ]
             st.dataframe(df)
