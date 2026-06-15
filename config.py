9882639915966cc118ee41d6cded82bd424c251c
wassannc <balaram@wassan.org>
2026-06-15 11:08:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,16 +23,6 @@ FORMS = {
         "column_labels": {"new_irri_structr-block": "Block","new_irri_structr-gp": "GP","new_irri_structr-village": "Village","new_irri_structr-villages_under_proposed_system": "No of villages covered","new_irri_structr-Source_name": "Water source","new_irri_structr-source_elevation": "Elevation of source","new_irri_structr-Ayacut_elevation": "elevation of ayacut","new_irri_structr-in_hieght": "Which is in height","new_irri_structr-distance_proposed_site_to_ayacut_mtrs": "Distance- site to ayacut/meters","new_irri_structr_-families_covered_under_irrigation": "Families covered","new_irri_structr_-total_land_village_acrs": "Total village land-acres","new_irri_structr_-proposed_ayacut_acrs": "Proposed ayacut-acres","enumerator-Jalamithra": "Jalamithra"},
         "landscape_col": "new_irri_structr-village"
     },
-    "4-New Farmpond": {
-        "form_id": "4-New Farmpond",
-        "columns": ["CB_info-landscape","CB_info-gp","CB_info-village","CB_info-Trainining_type","CB_info-Event_name","CB_info-Event_mode","Cb_info1-from_date","Cb_info1-days","Cb_info1-male","Cb_info1-female","Cb_info1-total_members","Cb_info1-Event_place"],
-        "landscape_col": "CB_info-landscape"
-    },
-    "5-Trench Works": {
-        "form_id": "5-SCT",
-        "columns": ["SubmissionDate","basic_info-landscape","basic_info-gp","basic_info-village","basic_info-orchard_type","basic_info-farmer_add","type"],
-        "landscape_col": "basic_info-landscape"
-    },
     "6. Lead_statement": {
         "form_id": "6. Lead_statement",
         "columns": ["lead_statement-block","lead_statement-gp","lead_statement-village","sand-plainroad_kms_from_Kothavala","sand-ghatroad_kms_from_Kothavalas","metal-plainroad_kms_mamidipalli_metal","metal-ghatroad_kms_mamidipalli_metal","ud-chc_equipmnt_hired_farmer","ud-chc_equipmnt_total_hours_used","stone-place_stone","enumerator-Jalamithra"],
